16e77dfe01216b1aad843cc8585b2ad20d06b003
Lalala6982 <131225145+Lalala6982@users.noreply.github.com>
2024-05-09 23:07:59 -0700

    update visiblity

--- flagcamp/src/components/Tracking.js
+++ flagcamp/src/components/Tracking.js
@@ -5,11 +5,14 @@ function Tracking() {
   const [loading, setLoading] = useState(false);
   const [trackingID, setTrackingID] = useState("");
   const [trackingStatus, setTrackingStatus] = useState([]);
+  const [isVisible, setIsVisible] = useState(false);
+
   const handleTrack = () => {
     if (trackingID === "") {
       return;
     }
     setLoading(true);
+    setIsVisible(true);
     // try {
     //   const data = await getTrackingStatus(trackingID);
     //   setTrackingStatus(data.result);
@@ -17,6 +20,7 @@ function Tracking() {
     //   message.error(error.message);
     // } finally {
     //   setLoading(false);
+    //   setIsVisible(false;)
     // }
   };
 
@@ -34,34 +38,35 @@ function Tracking() {
             },
           ]}
         >
-        <Input.Group compact>
-          <Input
-            style={{ width: 450, marginRight: 5 }}
-            placeholder="Enter your Tracking ID"
-            value={trackingID}
-            onChange={(e) => setTrackingID(e.target.value)}
-            onPressEnter={handleTrack}
+          <Input.Group compact>
+            <Input
+              style={{ width: 450, marginRight: 5 }}
+              placeholder="Enter your Tracking ID"
+              value={trackingID}
+              onChange={(e) => setTrackingID(e.target.value)}
+              onPressEnter={handleTrack}
+            />
+            <Button type="primary" onClick={handleTrack}>
+              Track
+            </Button>
+          </Input.Group>
+          <List
+            loading={loading}
+            style={{
+              marginTop: 20,
+              height: "calc(100% - 30px)",
+              overflow: "auto",
+              display: isVisible ? "block" : "none",
+            }}
+            grid={{ gutter: 1, xs: 1, sm: 1, md: 1, lg: 1, xl: 1, xxl: 1 }}
+            dataSource={[1]}
+            renderItem={(currentStatus) => (
+              <List.Item key={currentStatus}>
+                <Card title={currentStatus} />
+              </List.Item>
+            )}
           />
-          <Button type="primary" onClick={handleTrack}>
-            Track
-          </Button>
-        </Input.Group>
-        <List
-          loading={loading}
-          style={{
-            marginTop: 20,
-            height: "calc(100% - 30px)",
-            overflow:"auto"
-          }}
-          grid={{ gutter: 1, xs: 1, sm: 1, md: 1, lg: 1, xl: 1, xxl: 1 }}
-          dataSource={[1]}
-          renderItem={(currentStatus) => (
-            <List.Item key={currentStatus}>
-              <Card title={currentStatus} />
-            </List.Item>
-          )}
-          />
-          </Form.Item>
+        </Form.Item>
       </Form>
     </Row>
   );
