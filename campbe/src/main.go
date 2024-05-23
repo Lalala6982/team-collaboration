fa31f2fc25d3bfb81ea58b98926a4440c4b4d3e1
Lalala6982 <131225145+Lalala6982@users.noreply.github.com>
2024-05-23 00:59:28 -0700

    modified:   campbe/src/main.go

--- campbe/src/main.go
+++ campbe/src/main.go
@@ -12,5 +12,9 @@ import (
 
 func main() {
     fmt.Println("started-service")
+	origin := "New York, NY"
+    destination := "Washington, DC"
+	gateway.GetRobotRoute(origin, destination)
+	gateway.GetDroneRoute(origin, destination)
     // log.Fatal(http.ListenAndServe(":8080", handler.InitRouter()))
 }
